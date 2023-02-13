53b10f9739f53a9b909ea4e72d56479c0fd803f1
David4bay <david4bay@gmail.com>
2023-02-14 00:13:27 +0100

    theme fixes

--- script/script.js
+++ script/script.js
@@ -3,12 +3,12 @@ const switchButton = document.querySelector(".theme-toggle");
 const body = document.querySelector('body');
 const main = document.querySelector('main');
 const mainCards = document.querySelectorAll('.social-card')
-const userColor = document.querySelectorAll('h1.users-engaged');
+const userColor = document.querySelectorAll('.users-engaged');
 
 toggle.addEventListener('change', function () {
-  body.classList.toggle('dark-main');
-  switchButton.classList.toggle('active-switch');
-  for (let i = 0; i < mainCards.length; i++) {
-    mainCards[i].classList.toggle('text-white');
-  }
+    switchButton.classList.toggle('active-switch');
+    body.classList.toggle('dark-main');
+for (let i = 0; i < userColor.length; i++) {
+    user
+} 
 });
