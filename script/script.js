18d088ad18159d9dd3644763c546ccebf29e7f0f
David4bay <david4bay@gmail.com>
2023-02-14 14:27:06 +0100

    fixes

--- script/script.js
+++ script/script.js
@@ -2,13 +2,15 @@ const toggle = document.querySelector("#theme");
 const switchButton = document.querySelector(".theme-toggle");
 const body = document.querySelector('body');
 const main = document.querySelector('main');
-const mainCards = document.querySelectorAll('.social-card')
+const mainCards = document.querySelectorAll('.social-card');
 const userColor = document.querySelectorAll('.users-engaged');
 
-toggle.addEventListener('change', function () {
-    switchButton.classList.toggle('active-switch');
-    body.classList.toggle('dark-main');
-for (let i = 0; i < userColor.length; i++) {
-    user
-} 
-});
+$(document).ready(function() {
+    $(toggle).change(function() {
+        $(body).toggleClass('dark-main');
+        $(switchButton).toggleClass('active-switch');
+        $(userColor).each(function() {
+            $(this).toggleClass('font-color');
+        })
+    })
+})
