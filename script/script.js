1481ff4050d0a7347add3927e84f408d5ec69127
David4bay <david4bay@gmail.com>
2023-02-14 21:49:08 +0100

    fixed layout

--- script/script.js
+++ script/script.js
@@ -1,16 +1,29 @@
-const toggle = document.querySelector("#theme");
-const switchButton = document.querySelector(".theme-toggle");
-const body = document.querySelector('body');
-const main = document.querySelector('main');
-const mainCards = document.querySelectorAll('.social-card');
-const userColor = document.querySelectorAll('h1');
+const root = document.documentElement.style;
+const switchButton = document.querySelector('.theme-toggle');
+let mode = 'dark';
 
-$(document).ready(function() {
-    $(toggle).change(function() {
-        $(body).toggleClass('dark-main');
-        $(switchButton).toggleClass('active-switch');
-        $(userColor).each(function() {
-            $(this).toggleClass('dark-main');
-        })
-    })
-})
+switchButton.addEventListener('change', () => {
+    mode === 'dark' ? lightMode() : darkMode();
+});
+
+function darkMode() {
+    mode = 'dark';
+    root.setProperty('--background', 'hsl(0, 0%, 100%)');
+    root.setProperty('--pattern', 'hsl(225, 100%, 98%)');
+    root.setProperty('--card-bg', 'hsl(227, 47%, 96%)');
+    root.setProperty('--primary-text', 'hsl(228, 12%, 44%)');
+    root.setProperty('--secondary-text', 'hsl(230, 17%, 14%)');
+    root.setProperty('--toggle-gradient', 'hsl(230, 22%, 74%)');
+    root.setProperty('--overview', 'hsl(228, 12%, 44%)');
+}
+
+function lightMode() {
+    mode = 'light';
+    root.setProperty('--background', 'hsl(230, 17%, 14%)');
+    root.setProperty('--pattern', 'hsl(232, 19%, 15%)');
+    root.setProperty('--card-bg', 'hsl(228, 28%, 20%)');
+    root.setProperty('--primary-text', 'hsl(228, 34%, 66%)');
+    root.setProperty('--secondary-text', 'hsl(0, 0%, 100%)');
+    root.setProperty('--toggle-gradient', 'linear-gradient(45deg, hsl(210, 78%, 56%), hsl(146, 68%, 55%))');
+    root.setProperty('--overview', 'hsl(0, 0%, 100%)');
+}
